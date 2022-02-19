d9db005a5777ec06751601ca29a8091f9e965015
SANTIKO KUSNUL HAKIM <samokit@gmail.com>
2022-02-19 10:26:41 +0700

    Add files via upload

---
+++ klik.js
@@ -0,0 +1,3 @@
+var jq = document.createElement('script');
+jq.src = "https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.8/sweetalert2.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
---
+++ skrip.js
@@ -0,0 +1,3 @@
+setInterval(function(){
+	document.getElementsByClassName("ytp-ad-skip-button ytp-button")[0].click();
+},3000);
